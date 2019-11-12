b88d0071b72eceace6f36b50b58b5af3f0e18995
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-11-11 21:55:22 -0300

    Add the assets server and a middleware
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- server.go
+++ server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -12,24 +15,55 @@ func Serve(address string, port string) {
 
 	log.Printf("[+] Serving on %s:%s\n", address, port)
 
-	r := mux.NewRouter()
+	router := mux.NewRouter()
 
-	r.HandleFunc("/", HandleIndexPage)
+	router.HandleFunc("/", HandleIndexPage)
 
-	r.HandleFunc("/help", HandleHelpPage)
-	r.HandleFunc("/about", HandleAboutPage)
-	r.HandleFunc("/register", HandleRegisterPage)
-	r.HandleFunc("/", HandleIndexPage)
+	router.HandleFunc("/help", HandleHelpPage)
+	router.HandleFunc("/about", HandleAboutPage)
+	router.HandleFunc("/register", HandleRegisterPage)
 
-	err := http.ListenAndServe(address+":"+port, nil)
+	// REST API
+	//r.HandleFunc("/v1/people/")
+	//r.HandleFunc("/v1/people/{action}")
+
+	//r.HandleFunc("/v1/hardware/")
+	//r.HandleFunc("/v1/hardware/{action}")
+	//r.HandleFunc("/v1/hardware/{[0-9]}/{action}")
+
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/",
+		http.FileServer(http.Dir("./html/assets/"))))
+	router.Use(MiddlewareStaticFiles)
+
+	err := http.ListenAndServe(address+":"+port, router)
 
 	if err != nil {
-		log.Fatalf("[-] Server initialization error")
+		log.Fatalf("[!] Server initialization error. Reason %v", err)
 	}
 }
 
-func HandleIndexPage(w http.ResponseWriter, r *http.Request) {
+// MiddlewareStaticFiles fgn
+func MiddlewareStaticFiles(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Only do work if the request is for assets
+		if strings.Contains(r.URL.String(), "assets") {
+			if path.Ext(r.URL.String()) == "" || r.Header.Get("Referer") == "" {
+				return
+			}
+		}
 
+		h.ServeHTTP(w, r)
+	})
+}
+
+// HandleIndexPage
+func HandleIndexPage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/external.html",
+		"html/pages/index.html",
+	))
+
+	t.Execute(w, nil)
 }
 
 func HandleRegisterPage(w http.ResponseWriter, r *http.Request) {
