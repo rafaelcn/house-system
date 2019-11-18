342d216951d25249ac69d36a19989c709d8d9308
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-11-18 07:11:08 -0300

    Add an entitites that map objects in the database
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ entitites.go
@@ -0,0 +1,23 @@
+package main
+
+type (
+	House struct {
+		ID int
+		Name string
+	}
+
+	User struct {
+		ID int
+		Name string
+		Mail string
+		Password string
+		Phone string
+		Birth string
+		Type int
+	}
+
+	Object struct {
+
+	}
+
+)
