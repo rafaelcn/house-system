edeb407f7dba141a25f2cf3b81aebe339cce679c
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-11-18 07:07:50 -0300

    Add new REST routes and update page routes
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- server.go
+++ server.go
@@ -21,14 +21,23 @@ func Serve(address string, port string) {
 
 	router.HandleFunc("/help", HandleHelpPage)
 	router.HandleFunc("/about", HandleAboutPage)
-	router.HandleFunc("/register", HandleRegisterPage)
+	router.HandleFunc("/sign-up", HandleRegisterPage)
+	router.HandleFunc("/login", HandleLoginPage)
+
+	// Internal pages
+	router.HandleFunc("/homepage", HandleHomePage)
+	router.HandleFunc("/settings", HandleSettingsPage)
+	router.HandleFunc("/user-settings", HandleUserSettingsPage)
+	router.HandleFunc("/acessory", HandleAcessoryPage)
 
 	// REST API
+	router.HandleFunc("/v1/login", HandleLogin)
+	router.HandleFunc("/v1/logout", HandleLogout)
+
 	router.HandleFunc("/v1/user/{id}", HandleUser)
 	router.HandleFunc("/v1/user/{action}", HandleUser)
 
 	router.HandleFunc("/v1/people", HandlePeople)
-	//router.HandleFunc("/v1/people/{action}", HandlePeople)
 
 	router.HandleFunc("/v1/object/{id}", HandleObject)
 	router.HandleFunc("/v1/object/{action}", HandleObject)
@@ -59,41 +68,119 @@ func MiddlewareStaticFiles(h http.Handler) http.Handler {
 	})
 }
 
-// HandleIndexPage
+// HandleIndexPage ...
 func HandleIndexPage(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles(
 		"html/templates/external.html",
-		"html/pages/index.html",
+		"html/pages/external/index.html",
 	))
 
-	t.Execute(w, nil)
+	d := PageData {
+		Title: "Início",
+	}
+
+	t.Execute(w, d)
 }
 
-// HandleRegisterPage
+// HandleLoginPage ...
+func HandleLoginPage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/external.html",
+		"html/pages/external/login.html",
+	))
+
+	d := PageData {
+		Title: "Login",
+	}
+
+	t.Execute(w, d)
+}
+
+// HandleRegisterPage ...
 func HandleRegisterPage(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles(
 		"html/templates/external.html",
-		"html/pages/register.html",
+		"html/pages/external/register.html",
 	))
 
-	t.Execute(w, nil)
+	d := PageData {
+		Title: "Registrar-se",
+	}
+
+	t.Execute(w, d)
 }
 
-// HandleHelpPage
-func HandleHelpPage(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles(
-		"html/templates/external.html",
-		"html/pages/help.html",
-	))
-
-	t.Execute(w, nil)
-}
-
-// HandleAboutPage
+// HandleAboutPage ...
 func HandleAboutPage(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles(
 		"html/templates/external.html",
-		"html/pages/about.html",
+		"html/pages/external/about.html",
+	))
+
+	d := PageData {
+		Title: "Sobre",
+	}
+
+	t.Execute(w, d)
+}
+
+// Internal pages are defined below
+
+// HandleHomePage ...
+func HandleHomePage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/internal.html",
+		"html/pages/internal/home.html",
+	))
+
+	t.Execute(w, nil)
+}
+
+// HandleSettingsPage ...
+func HandleSettingsPage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/internal.html",
+		"html/pages/internal/settings.html",
+	))
+
+	t.Execute(w, nil)
+}
+
+// HandleSettingsPage ...
+func HandleUserSettingsPage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/internal.html",
+		"html/pages/internal/user_settings.html",
+	))
+
+	t.Execute(w, nil)
+}
+
+// HandleAddAcessoryPage ...
+func HandleAddAcessoryPage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/internal.html",
+		"html/pages/internal/add_acessory.html",
+	))
+
+	t.Execute(w, nil)
+}
+
+// HandleAcessoryPage ...
+func HandleAcessoryPage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/internal.html",
+		"html/pages/internal/acessory.html",
+	))
+
+	t.Execute(w, nil)
+}
+
+// HandleHelpPage ...
+func HandleHelpPage(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"html/templates/internal.html",
+		"html/pages/internal/help.html",
 	))
 
 	t.Execute(w, nil)
