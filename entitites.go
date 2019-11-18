2ee99e40ddd0dfc626da4e75d2f215e6b5b9f7ad
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-11-18 19:43:50 -0300

    Rest API for object fetching is complete
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- entitites.go
+++ entitites.go
@@ -1,23 +1,35 @@
 package main
 
 type (
+	// House ...
 	House struct {
 		ID int
 		Name string
 	}
 
+	// User ..
 	User struct {
 		ID int
 		Name string
 		Mail string
+		Username string
 		Password string
 		Phone string
 		Birth string
 		Type int
 	}
 
+	// Object ...
 	Object struct {
-
+		ID string
+		Name string
+		Status bool
+		Type int
+		House int
+		Intensity interface{}
+		Distance interface{}
+		Volume interface{}
+		Temperature interface{}
 	}
 
 )
