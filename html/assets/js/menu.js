d0c615fc0a19d0a699917925dcac6152409bbe7b
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-11-19 21:21:35 -0300

    Implements every thing with client requests
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- html/assets/js/menu.js
+++ html/assets/js/menu.js
@@ -1,5 +1,4 @@
 $(document).ready(() => {
-
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
