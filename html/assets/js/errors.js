e8d9174d19ca6e2f986d9111f591004f3bb8eb6a
Rafael Campos Nunes <rafaelnunes@engineer.com>
2019-11-19 21:19:34 -0300

    Add system errors corresponding with the server
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ html/assets/js/errors.js
@@ -0,0 +1,6 @@
+// Known errors of the system
+
+const errorIncompleteRequest = 0x00F1
+const errorNotFullfilledRequest = 0x00F2
+const errorDatabaseResponse = 0x00F3
+const errorNotAuthorized = 0x00F4
